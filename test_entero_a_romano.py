d4d63f4a2a8226ff878fca3db65686b67f2efb77
Alvaritto951 <alvaritto951@gmail.com>
2022-07-19 21:29:27 +0200

    Mi primer commit

---
+++ test_entero_a_romano.py
@@ -0,0 +1,25 @@
+from first_part import entero_a_romano
+
+"""
+Casos de prueba
+a) 1994 -> MCMXCIV
+b) 4000 -> RomanNumberError("El valor debe ser menor de 4000")
+c) "unacadena" -> RomanNumberError("Debe ser un entero")
+d) 0 -> RomanNumberError("El valor debe ser mayor de cero")
+e) -3 -> RomanNumberError("El valor debe ser mayor de cero")
+f) 4.5 -> RomanNumberError("Debe ser un entero")
+
+
+"""
+
+
+
+def test_3_es_igual_a_2_mas_uno():
+    assert 2 + 1 == 3
+
+
+def test_error_si_entero_mayor_de_3999():
+    assert entero_a_romano(1994) == 'MCMXCIV'
+
+def teast_descomposicion_336():
+    assert entero_a_romano(336) == ['0000', '300', '30', '6'] #=> 'CCCXXXVI'
